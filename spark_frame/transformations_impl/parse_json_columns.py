aee9f4737e20590c5137696697298d191bc0e07c
fpin <pin.furcy@gmail.com>
2022-11-30 17:17:11 +0100

    refactor: improve perf of parse_json_columns
    When parsing multiple columns, instead of doing each cycle
    wrapping - schema detection - parsing
    sequentially one column at a time, we now do each step
    for all columns at once before performing the next.
    This allows us to parallelize the first and last step, since
    only the schema detection requires an action.

--- spark_frame/transformations_impl/parse_json_columns.py
+++ spark_frame/transformations_impl/parse_json_columns.py
@@ -2,6 +2,7 @@ from typing import Dict, List, Union
 
 from pyspark.sql import Column, DataFrame
 from pyspark.sql import functions as f
+from pyspark.sql.types import StructType
 
 from spark_frame.utils import quote
 
@@ -24,7 +25,136 @@ def parse_json_columns(df: DataFrame, columns: Union[str, List[str], Dict[str, s
     WARNING : when you use this method on a column that is inside a struct (e.g. column "a.b.c"),
     instead of replacing that column it will create a new column outside the struct (e.g. "`a.b.c`") (See Example 2).
 
-    Example 1 :
+    Args:
+        df: A Spark DataFrame
+        columns: A column name, list of column names, or dict(column_name, parsed_column_name)
+
+    Returns:
+        A new DataFrame
+
+    Examples:
+
+        **Example 1 :**
+        >>> from pyspark.sql import SparkSession
+        >>> spark = SparkSession.builder.appName("doctest").getOrCreate()
+        >>> df = spark.createDataFrame([
+        ...         (1, '[{"a": 1}, {"a": 2}]'),
+        ...         (1, '[{"a": 2}, {"a": 4}]'),
+        ...         (2, None)
+        ...     ], "id INT, json1 STRING"
+        ... )
+        >>> df.show()
+        +---+--------------------+
+        | id|               json1|
+        +---+--------------------+
+        |  1|[{"a": 1}, {"a": 2}]|
+        |  1|[{"a": 2}, {"a": 4}]|
+        |  2|                null|
+        +---+--------------------+
+        <BLANKLINE>
+        >>> df.printSchema()
+        root
+         |-- id: integer (nullable = true)
+         |-- json1: string (nullable = true)
+        <BLANKLINE>
+        >>> parse_json_columns(df, 'json1').printSchema()
+        root
+         |-- id: integer (nullable = true)
+         |-- json1: array (nullable = true)
+         |    |-- element: struct (containsNull = true)
+         |    |    |-- a: long (nullable = true)
+        <BLANKLINE>
+
+        **Example 2 : different output column name :**
+        >>> parse_json_columns(df, {'json1': 'parsed_json1'}).printSchema()
+        root
+         |-- id: integer (nullable = true)
+         |-- json1: string (nullable = true)
+         |-- parsed_json1: array (nullable = true)
+         |    |-- element: struct (containsNull = true)
+         |    |    |-- a: long (nullable = true)
+        <BLANKLINE>
+
+        **Example 3 : json inside a struct :**
+        >>> df = spark.createDataFrame([
+        ...         (1, {'json1': '[{"a": 1}, {"a": 2}]'}),
+        ...         (1, {'json1': '[{"a": 2}, {"a": 4}]'}),
+        ...         (2, None)
+        ...     ], "id INT, struct STRUCT<json1: STRING>"
+        ... )
+        >>> df.show(10, False)
+        +---+----------------------+
+        |id |struct                |
+        +---+----------------------+
+        |1  |{[{"a": 1}, {"a": 2}]}|
+        |1  |{[{"a": 2}, {"a": 4}]}|
+        |2  |null                  |
+        +---+----------------------+
+        <BLANKLINE>
+        >>> df.printSchema()
+        root
+         |-- id: integer (nullable = true)
+         |-- struct: struct (nullable = true)
+         |    |-- json1: string (nullable = true)
+        <BLANKLINE>
+        >>> res = parse_json_columns(df, 'struct.json1')
+        >>> res.printSchema()
+        root
+         |-- id: integer (nullable = true)
+         |-- struct: struct (nullable = true)
+         |    |-- json1: string (nullable = true)
+         |-- struct.json1: array (nullable = true)
+         |    |-- element: struct (containsNull = true)
+         |    |    |-- a: long (nullable = true)
+        <BLANKLINE>
+        >>> res.show(10, False)
+        +---+----------------------+------------+
+        |id |struct                |struct.json1|
+        +---+----------------------+------------+
+        |1  |{[{"a": 1}, {"a": 2}]}|[{1}, {2}]  |
+        |1  |{[{"a": 2}, {"a": 4}]}|[{2}, {4}]  |
+        |2  |null                  |null        |
+        +---+----------------------+------------+
+        <BLANKLINE>
+
+    """
+    if isinstance(columns, str):
+        columns = [columns]
+    if isinstance(columns, list):
+        columns = {col: col for col in columns}
+
+    wrapped_df = __wrap_json_columns(df, columns)
+    schema_per_col = __infer_schema_per_column(wrapped_df, list(columns.values()))
+    res = __parse_json_columns(wrapped_df, schema_per_col)
+    return res
+
+
+def _wrap_json(col: str, parsed_col: str) -> Column:
+    """Transforms a string column into the json string '{"col_name": col_value}'
+
+    It is necessary to wrap the json because the schema inference is incorrect for json that are arrays
+
+    Examples:
+    df.show()
+    +---+--------------------+
+    | id|               json1|
+    +---+--------------------+
+    |  1|[{"a": 1}, {"a": 2}]|
+    |  1|[{"a": 2}, {"a": 4}]|
+    +---+--------------------+
+    schema = job.spark.read.json(df.select('json1').rdd.map(lambda r: r[0])).schema
+    schema.simpleString()
+    returns 'struct<a:bigint>' but the schema we need is 'array<struct<a:bigint>>'
+
+    :param col:
+    :return:
+    """
+    return f.concat(f.lit('{"'), f.lit(parsed_col), f.lit('": '), f.coalesce(col, f.lit("null")), f.lit("}"))
+
+
+def __wrap_json_columns(df: DataFrame, columns: Dict[str, str]) -> DataFrame:
+    """Wrap a json column inside a struct.
+
     >>> from pyspark.sql import SparkSession
     >>> spark = SparkSession.builder.appName("doctest").getOrCreate()
     >>> df = spark.createDataFrame([
@@ -42,21 +172,79 @@ def parse_json_columns(df: DataFrame, columns: Union[str, List[str], Dict[str, s
     |  2|                null|
     +---+--------------------+
     <BLANKLINE>
-    >>> df.printSchema()
-    root
-     |-- id: integer (nullable = true)
-     |-- json1: string (nullable = true)
-    <BLANKLINE>
-    >>> parse_json_columns(df, 'json1').printSchema()
-    root
-     |-- id: integer (nullable = true)
-     |-- json1: array (nullable = true)
-     |    |-- element: struct (containsNull = true)
-     |    |    |-- a: long (nullable = true)
+    >>> __wrap_json_columns(df, {"json1": "parsed_json1"}).show(truncate=False)
+    +---+--------------------+--------------------------------------+
+    |id |json1               |parsed_json1                          |
+    +---+--------------------+--------------------------------------+
+    |1  |[{"a": 1}, {"a": 2}]|{"parsed_json1": [{"a": 1}, {"a": 2}]}|
+    |1  |[{"a": 2}, {"a": 4}]|{"parsed_json1": [{"a": 2}, {"a": 4}]}|
+    |2  |null                |{"parsed_json1": null}                |
+    +---+--------------------+--------------------------------------+
     <BLANKLINE>
 
-    Example 2 : different output column name:
-    >>> parse_json_columns(df, {'json1': 'parsed_json1'}).printSchema()
+    """
+    res = df
+    for col, parsed_col in columns.items():
+        res = res.withColumn(parsed_col, _wrap_json(col, parsed_col))
+    return res
+
+
+def __infer_schema_per_column(df: DataFrame, columns: List[str]) -> Dict[str, StructType]:
+    """Infer the schema of all the specified columns
+
+    >>> from pyspark.sql import SparkSession
+    >>> spark = SparkSession.builder.appName("doctest").getOrCreate()
+    >>> df = spark.createDataFrame([
+    ...         (1, '[{"a": 1}, {"a": 2}]', '{"parsed_json1": [{"a": 1}, {"a": 2}]}'),
+    ...         (1, '[{"a": 2}, {"a": 4}]', '{"parsed_json1": [{"a": 2}, {"a": 4}]}'),
+    ...         (2, None, '{"parsed_json1": null}'),
+    ...     ], "id INT, json1 STRING, parsed_json1 STRING"
+    ... )
+    >>> df.show(truncate=False)
+    +---+--------------------+--------------------------------------+
+    |id |json1               |parsed_json1                          |
+    +---+--------------------+--------------------------------------+
+    |1  |[{"a": 1}, {"a": 2}]|{"parsed_json1": [{"a": 1}, {"a": 2}]}|
+    |1  |[{"a": 2}, {"a": 4}]|{"parsed_json1": [{"a": 2}, {"a": 4}]}|
+    |2  |null                |{"parsed_json1": null}                |
+    +---+--------------------+--------------------------------------+
+    <BLANKLINE>
+    >>> __infer_schema_per_column(df, ["parsed_json1"])["parsed_json1"].simpleString()
+    'struct<parsed_json1:array<struct<a:bigint>>>'
+
+    """
+    spark = df.sparkSession
+    schema_per_col = {}
+    for col in columns:
+        schema = spark.read.json(df.select(quote(col)).rdd.map(lambda r: r[0])).schema
+        schema_per_col[col] = schema
+    return schema_per_col
+
+
+def __parse_json_columns(df: DataFrame, schema_per_col: Dict[str, StructType]) -> DataFrame:
+    """Infer the schema of all the specified columns
+
+    >>> from pyspark.sql import SparkSession
+    >>> spark = SparkSession.builder.appName("doctest").getOrCreate()
+    >>> df = spark.createDataFrame([
+    ...         (1, '[{"a": 1}, {"a": 2}]', '{"parsed_json1": [{"a": 1}, {"a": 2}]}'),
+    ...         (1, '[{"a": 2}, {"a": 4}]', '{"parsed_json1": [{"a": 2}, {"a": 4}]}'),
+    ...         (2, None, '{"parsed_json1": null}'),
+    ...     ], "id INT, json1 STRING, parsed_json1 STRING"
+    ... )
+    >>> df.show(truncate=False)
+    +---+--------------------+--------------------------------------+
+    |id |json1               |parsed_json1                          |
+    +---+--------------------+--------------------------------------+
+    |1  |[{"a": 1}, {"a": 2}]|{"parsed_json1": [{"a": 1}, {"a": 2}]}|
+    |1  |[{"a": 2}, {"a": 4}]|{"parsed_json1": [{"a": 2}, {"a": 4}]}|
+    |2  |null                |{"parsed_json1": null}                |
+    +---+--------------------+--------------------------------------+
+    <BLANKLINE>
+    >>> from spark_frame.schema_utils import schema_from_simple_string
+    >>> schema = schema_from_simple_string('struct<parsed_json1:array<struct<a:bigint>>>')
+    >>> res = __parse_json_columns(df, {"parsed_json1": schema})
+    >>> res.printSchema()
     root
      |-- id: integer (nullable = true)
      |-- json1: string (nullable = true)
@@ -64,117 +252,18 @@ def parse_json_columns(df: DataFrame, columns: Union[str, List[str], Dict[str, s
      |    |-- element: struct (containsNull = true)
      |    |    |-- a: long (nullable = true)
     <BLANKLINE>
-
-    Example 3 : json inside a struct :
-    >>> df = spark.createDataFrame([
-    ...         (1, {'json1': '[{"a": 1}, {"a": 2}]'}),
-    ...         (1, {'json1': '[{"a": 2}, {"a": 4}]'}),
-    ...         (2, None)
-    ...     ], "id INT, struct STRUCT<json1: STRING>"
-    ... )
-    >>> df.show(10, False)
-    +---+----------------------+
-    |id |struct                |
-    +---+----------------------+
-    |1  |{[{"a": 1}, {"a": 2}]}|
-    |1  |{[{"a": 2}, {"a": 4}]}|
-    |2  |null                  |
-    +---+----------------------+
-    <BLANKLINE>
-    >>> df.printSchema()
-    root
-     |-- id: integer (nullable = true)
-     |-- struct: struct (nullable = true)
-     |    |-- json1: string (nullable = true)
-    <BLANKLINE>
-    >>> res = parse_json_columns(df, 'struct.json1')
-    >>> res.printSchema()
-    root
-     |-- id: integer (nullable = true)
-     |-- struct: struct (nullable = true)
-     |    |-- json1: string (nullable = true)
-     |-- struct.json1: array (nullable = true)
-     |    |-- element: struct (containsNull = true)
-     |    |    |-- a: long (nullable = true)
-    <BLANKLINE>
-    >>> res.show(10, False)
-    +---+----------------------+------------+
-    |id |struct                |struct.json1|
-    +---+----------------------+------------+
-    |1  |{[{"a": 1}, {"a": 2}]}|[{1}, {2}]  |
-    |1  |{[{"a": 2}, {"a": 4}]}|[{2}, {4}]  |
-    |2  |null                  |null        |
-    +---+----------------------+------------+
+    >>> res.show(truncate=False)
+    +---+--------------------+------------+
+    |id |json1               |parsed_json1|
+    +---+--------------------+------------+
+    |1  |[{"a": 1}, {"a": 2}]|[{1}, {2}]  |
+    |1  |[{"a": 2}, {"a": 4}]|[{2}, {4}]  |
+    |2  |null                |null        |
+    +---+--------------------+------------+
     <BLANKLINE>
 
-    :param df: a DataFrame
-    :param columns: A column name, list of column names, or dict(column_name, parsed_column_name)
-    :return: a new DataFrame
     """
-    spark = df.sparkSession
-    if isinstance(columns, str):
-        columns = [columns]
-    if isinstance(columns, list):
-        columns = {col: col for col in columns}
-
-    def wrap_json(col: str, parsed_col: str) -> Column:
-        """Transforms a string column into the json string '{"col_name": col_value}'
-
-        It is necessary to wrap the json because the schema inference is incorrect for json that are arrays
-
-        Examples:
-        df.show()
-        +---+--------------------+
-        | id|               json1|
-        +---+--------------------+
-        |  1|[{"a": 1}, {"a": 2}]|
-        |  1|[{"a": 2}, {"a": 4}]|
-        +---+--------------------+
-        schema = job.spark.read.json(df.select('json1').rdd.map(lambda r: r[0])).schema
-        schema.simpleString()
-        returns 'struct<a:bigint>' but the schema we need is 'array<struct<a:bigint>>'
-
-        :param col:
-        :return:
-        """
-        return f.concat(f.lit('{"'), f.lit(parsed_col), f.lit('": '), f.coalesce(col, f.lit("null")), f.lit("}"))
-
     res = df
-    for col, parsed_col in columns.items():
-        # >>> res.show()
-        # +---+--------------------+
-        # | id|               json1|
-        # +---+--------------------+
-        # |  1|[{"a": 1}, {"a": 2}]|
-        # |  1|[{"a": 2}, {"a": 4}]|
-        # |  2|                null|
-        # +---+--------------------+
-        res = res.withColumn(parsed_col, wrap_json(col, parsed_col))
-        # >>> res.show(10, False)
-        # +---+--------------------+--------------------------------------+
-        # |id |json1               |parsed_json1                          |
-        # +---+--------------------+--------------------------------------+
-        # |1  |[{"a": 1}, {"a": 2}]|{"parsed_json1": [{"a": 1}, {"a": 2}]}|
-        # |1  |[{"a": 2}, {"a": 4}]|{"parsed_json1": [{"a": 2}, {"a": 4}]}|
-        # |2  |null                |{"parsed_json1": null}                |
-        # +---+--------------------+--------------------------------------+
-        schema = spark.read.json(res.select(quote(parsed_col)).rdd.map(lambda r: r[0])).schema
-        # >>> schema.simpleString()
-        # 'struct<parsed_json1:array<struct<a:bigint>>>'
+    for parsed_col, schema in schema_per_col.items():
         res = res.withColumn(parsed_col, f.from_json(quote(parsed_col), schema).alias(parsed_col)[parsed_col])
-        # >>> res.printSchema()
-        # root
-        #  |-- id: integer (nullable = true)
-        #  |-- json1: string (nullable = true)
-        #  |-- parsed_json1: array (nullable = true)
-        #  |    |-- element: struct (containsNull = true)
-        #  |    |    |-- a: long (nullable = true)
-        # >>> res.show()
-        # +---+--------------------+------------+
-        # | id|               json1|parsed_json1|
-        # +---+--------------------+------------+
-        # |  1|[{"a": 1}, {"a": 2}]|  [{1}, {2}]|
-        # |  1|[{"a": 2}, {"a": 4}]|  [{2}, {4}]|
-        # |  2|                null|        null|
-        # +---+--------------------+------------+
     return res
