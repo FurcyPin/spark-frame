105e7787a33bf9d3e0c3dda533f3584943b251b6
fpin <pin.furcy@gmail.com>
2022-12-21 16:09:00 +0100

    feat: add nested.select

--- spark_frame/nested.py
+++ spark_frame/nested.py
@@ -1,6 +1,7 @@
 from spark_frame.nested_impl.fields import fields
 from spark_frame.nested_impl.print_schema import print_schema
 from spark_frame.nested_impl.schema_string import schema_string
+from spark_frame.nested_impl.select_impl import select
 from spark_frame.nested_impl.unnest_all_fields import unnest_all_fields
 from spark_frame.nested_impl.unnest_field import unnest_field
 from spark_frame.nested_impl.with_fields import with_fields
@@ -8,6 +9,7 @@ from spark_frame.nested_impl.with_fields import with_fields
 fields = fields
 print_schema = print_schema
 schema_string = schema_string
+select = select
 unnest_all_fields = unnest_all_fields
 unnest_field = unnest_field
 with_fields = with_fields
