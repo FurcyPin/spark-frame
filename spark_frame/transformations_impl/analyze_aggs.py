0785d894021c3d0182d27414bf65276d5ca816d6
fpin <pin.furcy@gmail.com>
2023-01-26 15:12:19 +0100

    clean: rename function argument in analyze_aggs

--- spark_frame/transformations_impl/analyze_aggs.py
+++ spark_frame/transformations_impl/analyze_aggs.py
@@ -7,33 +7,33 @@ def _to_string(col: Column):
     return col.cast("STRING")
 
 
-def column_number(col: str, schema_field: StructField, col_num: int) -> Column:  # NOSONAR
+def column_number(col: str, struct_field: StructField, col_num: int) -> Column:  # NOSONAR
     return f.lit(col_num).alias("column_number")
 
 
-def column_name(col: str, schema_field: StructField, col_num: int) -> Column:  # NOSONAR
-    return f.lit(schema_field.name).alias("column_name")
+def column_name(col: str, struct_field: StructField, col_num: int) -> Column:  # NOSONAR
+    return f.lit(struct_field.name).alias("column_name")
 
 
-def column_type(col: str, schema_field: StructField, col_num: int) -> Column:  # NOSONAR
-    return f.lit(schema_field.dataType.typeName().upper()).alias("column_type")
+def column_type(col: str, struct_field: StructField, col_num: int) -> Column:  # NOSONAR
+    return f.lit(struct_field.dataType.typeName().upper()).alias("column_type")
 
 
-def count(col: str, schema_field: StructField, col_num: int) -> Column:  # NOSONAR
+def count(col: str, struct_field: StructField, col_num: int) -> Column:  # NOSONAR
     return f.count(f.lit(1)).alias("count")
 
 
-def count_distinct(col: str, schema_field: StructField, col_num: int) -> Column:  # NOSONAR
+def count_distinct(col: str, struct_field: StructField, col_num: int) -> Column:  # NOSONAR
     return f.count_distinct(col).alias("count_distinct")
 
 
-def count_null(col: str, schema_field: StructField, col_num: int) -> Column:  # NOSONAR
+def count_null(col: str, struct_field: StructField, col_num: int) -> Column:  # NOSONAR
     return (f.count(f.lit(1)) - f.count(col)).alias("count_null")
 
 
-def min(col: str, schema_field: StructField, col_num: int) -> Column:  # NOSONAR
+def min(col: str, struct_field: StructField, col_num: int) -> Column:  # NOSONAR
     return _to_string(f.min(col)).alias("min")
 
 
-def max(col: str, schema_field: StructField, col_num: int) -> Column:  # NOSONAR
+def max(col: str, struct_field: StructField, col_num: int) -> Column:  # NOSONAR
     return _to_string(f.max(col)).alias("max")
