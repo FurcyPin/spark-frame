3492766304b77c0cadc1989a2ca84a3676f50c4a
Furcy Pin <pin.furcy@gmail.com>
2024-01-26 15:10:38 +0100

    feat!: remove DiffFormatOptions.max_string_length
    The option was not working anymore since the HTML report refactoring.
    It might be added back later, but it would be much
    more user-friendly if it could be changed dynamically
    directly in the HTML report, rather than requiring
    the user to re-generate a new HTML file every time.

--- spark_frame/data_diff/diff_format_options.py
+++ spark_frame/data_diff/diff_format_options.py
@@ -1,7 +1,6 @@
 from dataclasses import dataclass
 
 DEFAULT_NB_DIFFED_ROWS = 10
-DEFAULT_MAX_STRING_LENGTH = 30
 DEFAULT_LEFT_DF_ALIAS = "left"
 DEFAULT_RIGHT_DF_ALIAS = "right"
 
@@ -9,6 +8,5 @@ DEFAULT_RIGHT_DF_ALIAS = "right"
 @dataclass
 class DiffFormatOptions:
     nb_diffed_rows: int = DEFAULT_NB_DIFFED_ROWS
-    max_string_length: int = DEFAULT_MAX_STRING_LENGTH
     left_df_alias: str = DEFAULT_LEFT_DF_ALIAS
     right_df_alias: str = DEFAULT_RIGHT_DF_ALIAS
