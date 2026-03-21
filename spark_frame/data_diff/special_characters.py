35d298a1b0dd0224085017e003e5f664b7f4836e
Furcy Pin <pin.furcy@gmail.com>
2026-03-21 16:11:45 +0100

    deps!: add support for Spark 4.0 and 4.1, drop support for Spark 3.3 and 3.4

--- spark_frame/data_diff/special_characters.py
+++ spark_frame/data_diff/special_characters.py
@@ -31,14 +31,7 @@ def _restore_special_characters(col_name: str) -> str:
 
 
 def _replace_special_characters_from_col_names(df: DataFrame) -> DataFrame:
-    # TODO: remove this "if" once support for Spark 3.3 is dropped
-    if df.sparkSession.version >= "3.4":
-        return df.withColumnsRenamed({col: col.translate(_replacements) for col in df.columns})
-    else:
-        res_df = df
-        for col in df.columns:
-            res_df = res_df.withColumnRenamed(col, col.translate(_replacements))
-        return res_df
+    return df.withColumnsRenamed({col: col.translate(_replacements) for col in df.columns})
 
 
 def _restore_special_characters_from_col(col: Column) -> Column:
